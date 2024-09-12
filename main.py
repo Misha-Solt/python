3aa8b50323c646dde643180d4981ee48fde155d9
Misha-Solt <mikhail.us@gmail.com>
2024-09-12 09:23:40 +0200

    Inputs added.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # Print name, age and address to console
-name = 'Misha'
-address = 'Bavaria'
+# name = 'Misha'
+# address = 'Bavaria'
 
-print(name, 'live in', address)
+# print(name, 'live in', address)
 
-name = 'Mike'
-address = 'Bremen'
+name = input('Please input your name: ')
+address = input('Please input your address: ')
+age = input('Input your age: ')
 
-print(name, 'live in', address)
+print(name.capitalize(), 'is', age, 'years old and live in', address)
