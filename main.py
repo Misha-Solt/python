c0e49aa92a0d27a67d2046c5b82dbf33f484ae0b
Misha-Solt <mikhail.us@gmail.com>
2024-09-12 09:13:09 +0200

    My first code:)))

---
+++ main.py
@@ -0,0 +1,10 @@
+# Print name, age and address to console
+name = 'Misha'
+address = 'Bavaria'
+
+print(name, 'live in', address)
+
+name = 'Mike'
+address = 'Bremen'
+
+print(name, 'live in', address)
